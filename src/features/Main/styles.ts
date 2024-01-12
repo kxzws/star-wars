bc16e6e420770d51a249a67c02c61c7b51e27338
Alena Bulanova <kxzws54@gmail.com>
2024-01-12 15:00:04 +0100

    fix: fix background styles

--- src/features/Main/styles.ts
+++ src/features/Main/styles.ts
@@ -3,6 +3,7 @@ import { styled } from '@mui/system';
 import { MEDIA_ENDPOINTS } from '../../styles';
 
 export const Box = styled('div')`
+  min-height: calc(100vh - 76px);
   height: 100%;
   display: flex;
 
